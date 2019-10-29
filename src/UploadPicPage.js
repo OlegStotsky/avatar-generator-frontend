f6a7845238ff4c9620f274175ca9fc110607a914
Oleg Stotsky <ostotsky@gmail.com>
2019-10-29 13:03:11 +0300

    add spinner

--- src/UploadPicPage.js
+++ src/UploadPicPage.js
@@ -1,35 +1,36 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import { withStyles } from '@material-ui/styles';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 import Image from "material-ui-image";
+import Spinner from "react-spinner-material";
 import ApiAdapter from "./adapters/ApiAdapter";
 
 const styles = theme => ({
   button: {
-    margin: '10px',
-    width: '10px'
+    margin: "10px",
+    width: "10px"
   },
   input: {
-    display: 'none',
+    display: "none"
   },
   uploadPicPage: {
-    width: '600px'
+    width: "600px"
   },
   main: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh'
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh"
   },
   centered: {
-    display: 'flex',
-    justifyContent: 'center'
+    display: "flex",
+    justifyContent: "center"
   },
   button: {
-    margin: '10px'
-  },
+    margin: "10px"
+  }
 });
 
 class DisplayResultPage extends React.Component {
@@ -44,7 +45,7 @@ class DisplayResultPage extends React.Component {
     };
   }
 
-  onInputChange = (event) => {
+  onInputChange = event => {
     const { files } = event.target;
     const filePreview = URL.createObjectURL(files[0]);
     const fileReader = new FileReader();
@@ -54,21 +55,29 @@ class DisplayResultPage extends React.Component {
         filePreview,
         file: fileReader.result
       });
-    }
+    };
     this.setState({ targetPicWasLoaded: true });
-  }
-  
+  };
+
   startAgain = () => {
-    this.setState({ hasUploaded: false });
-  }
+    this.setState({
+      targetPicWasLoaded: false,
+      isUploading: true,
+      isSubmitting: false,
+      file: null,
+      generatedAvatar: null
+    });
+  };
 
   handleUpload = async () => {
     this.setState({ isSubmitting: true });
-    console.log("submitting");
-    console.log({ file: this.state.file });
     const response = await ApiAdapter.uploadProfilePicture(this.state.file);
-    this.setState({ isUploading: false, isSubmitting: false, generatedAvatar: `data:image/png;base64,${response}` });
-  }
+    this.setState({
+      isUploading: false,
+      isSubmitting: false,
+      generatedAvatar: `data:image/png;base64,${response}`
+    });
+  };
 
   render() {
     const { classes } = this.props;
@@ -76,7 +85,9 @@ class DisplayResultPage extends React.Component {
     return (
       <div className={classes.main}>
         <div className={classes.uploadPicPage}>
-          {this.state.isUploading ? this.renderBeforeUpload() : this.renderAfterUpload()}
+          {this.state.isUploading
+            ? this.renderBeforeUpload()
+            : this.renderAfterUpload()}
         </div>
       </div>
     );
@@ -86,11 +97,27 @@ class DisplayResultPage extends React.Component {
     const { classes } = this.props;
 
     return (
-      <Paper >
-        {this.state.file ? (<div><Image src={this.state.filePreview} /></div>) : 
-        (<Typography variant="h5" component="h3" className={classes.centered}>
-          Choose photo you would like to convert 
-        </Typography>)}
+      <Paper>
+        {this.state.file ? (
+          this.state.isSubmitting ? (
+            <div className={classes.centered}>
+              <Spinner
+                size={150}
+                spinnerColor={"#333"}
+                spinnerWidth={2}
+                visible={true}
+              />
+            </div>
+          ) : (
+            <div>
+              <Image src={this.state.filePreview} />
+            </div>
+          )
+        ) : (
+          <Typography variant="h5" component="h3" className={classes.centered}>
+            Choose photo you would like to convert
+          </Typography>
+        )}
         <input
           accept="image/*"
           className={classes.input}
@@ -100,16 +127,26 @@ class DisplayResultPage extends React.Component {
           onChange={this.onInputChange}
         />
         <div className={classes.centered}>
-        <label htmlFor="raised-button-file">
-          <Button component="span" variant="contained" className={classes.button}>
+          <label htmlFor="raised-button-file">
+            <Button
+              component="span"
+              variant="contained"
+              className={classes.button}
+            >
               select photo
-          </Button>
-        </label>
-          { this.state.targetPicWasLoaded &&  <Button variant="contained" className={classes.button} onClick={this.handleUpload}>
+            </Button>
+          </label>
+          {this.state.targetPicWasLoaded && (
+            <Button
+              variant="contained"
+              className={classes.button}
+              onClick={this.handleUpload}
+            >
               upload
-          </Button>}
+            </Button>
+          )}
         </div>
-    </Paper>
+      </Paper>
     );
   }
 
@@ -117,19 +154,24 @@ class DisplayResultPage extends React.Component {
     const { classes } = this.props;
 
     return (
-    <Paper >
-      <Typography variant="h5" component="h3" className={classes.centered}>
-        Here is your avatar 
-      </Typography>
-      <div>
-        <Image src={this.state.generatedAvatar} />
-      </div>
-      <div className={classes.centered}>
-        <Button variant="contained" className={classes.button} onClick={this.startAgain}>
+      <Paper>
+        <Typography variant="h5" component="h3" className={classes.centered}>
+          Here is your avatar
+        </Typography>
+        <div>
+          <Image src={this.state.generatedAvatar} />
+        </div>
+        <div className={classes.centered}>
+          <Button
+            variant="contained"
+            className={classes.button}
+            onClick={this.startAgain}
+          >
             Upload Another One
-        </Button>
-      </div>
-    </Paper>);
+          </Button>
+        </div>
+      </Paper>
+    );
   }
 }
 
